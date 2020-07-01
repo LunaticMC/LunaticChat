4cb8304936c10300b1f92515c3279f17cac65d09
lucy <lucy@lunaticmc.net>
2020-07-01 10:48:00 +0100

    JSON escaping, increment version, added GPL notice

--- src/main/java/xyz/poulton/lunaticchat/api/format/ChatFormat.java
+++ src/main/java/xyz/poulton/lunaticchat/api/format/ChatFormat.java
@@ -1,3 +1,19 @@
+//    Copyright © Lucy Poulton 2020.
+//    This file is part of LunaticChat.
+//
+//    LunaticChat is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    LunaticChat is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LunaticChat. If not, see <https://www.gnu.org/licenses/>.
+
 package xyz.poulton.lunaticchat.api.format;
 
 import me.clip.placeholderapi.PlaceholderAPI;
@@ -26,11 +42,9 @@ public class ChatFormat {
     }
 
     public BaseComponent[] resolve(Player sender, String message) {
+        message = message.replace("\\","\\\\");
+        message = message.replace("\"", "\\\"");
         String parsed = PlaceholderAPI.setPlaceholders(sender, template).replace("{MESSAGE}", message);
         return ComponentSerializer.parse(parsed);
-    }
-
-    public BaseComponent[] resolve(Player sender, BaseComponent[] message) {
-        return resolve(sender, message);
     }
 }
--- src/main/java/xyz/poulton/lunaticchat/bungee/LunaticChatBungee.java
+++ src/main/java/xyz/poulton/lunaticchat/bungee/LunaticChatBungee.java
@@ -1,3 +1,19 @@
+//    Copyright © Lucy Poulton 2020.
+//    This file is part of LunaticChat.
+//
+//    LunaticChat is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    LunaticChat is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LunaticChat. If not, see <https://www.gnu.org/licenses/>.
+
 package xyz.poulton.lunaticchat.bungee;
 
 import net.md_5.bungee.api.plugin.Plugin;
